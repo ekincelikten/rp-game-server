f0a19c0e7242ffbfff68515406d387828e2b11da
ekincelikten <ekinheisenberg@gmail.com>
2025-06-07 15:40:09 +0300

    Add files via upload

--- server_code.js
+++ server_code.js
@@ -107,6 +107,7 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3001, () => {
-  console.log('Server listening on port 3001');
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
